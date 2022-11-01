4fa2fc87a62fd661c0250b7f7d1705e39dd91b64
joemardev <joemarb.dev@gmail.com>
2022-11-01 16:21:23 +0800

    Design Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,18 @@ const ShowChildMenu = (elem) => {
 $('.memo-table td:odd').click(function() {
    $(this).parent().next().toggle();
 });
+
+const PagePopupHandler = () => $('.page-body-pop-up').toggle();
+
+const PageChildHandler = (elem, type) => {
+    $('.page-tab-menu-child').removeClass('active')
+    $(elem).addClass('active');
+
+    if(type == 1) {
+        $('.popup-child-content-1').show();
+        $('.popup-child-content-2').hide();
+    } if(type ==2) {
+        $('.popup-child-content-1').hide();
+        $('.popup-child-content-2').show();
+    }
+}
